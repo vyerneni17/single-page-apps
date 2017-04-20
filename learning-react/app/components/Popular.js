3aa7c5a79eb6169c0e3d0b05777e43112c7aa9ff
vyerneni17 <venkata.yerneni17@gmail.com>
2017-04-19 23:12:56 -0700

    React app to get the top repos on Github based on selected language

---
+++ learning-react/app/components/Popular.js
@@ -0,0 +1,105 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+var api = require('../utils/api');
+
+function SelectLanguage (props){
+  var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return(
+    <ul className='languages'>
+      { languages.map(function(lang){
+          return (
+            <li
+              style = {lang === props.selectedLanguage ? {color : '#d0021b'} : null}
+              onClick={props.onSelect.bind(null, lang)}
+              key={lang}>
+                {lang}
+            </li>
+          )
+        })}
+    </ul>
+  )
+}
+
+function RepoGrid(props){
+  return(
+    <ul className = 'popular-list'>
+      {props.repos.map(function(repo, index){
+        return (
+          <li key={repo.name} className='popular-name'>
+          <div className='popular-rank'>#{index + 1}</div>
+          <ul className='space-list-items'>
+            <li>
+              <img
+                className='avatar'
+                src = {repo.owner.avatar_url}
+                alt = {'Avatar for' + repo.owner.login}/>
+            </li>
+            <li><a href={repo.html_url}>{repo.name}</a></li>
+            <li>@{repo.owner.login}</li>
+            <li>{repo.stargazers_count} stars</li>
+          </ul>
+        </li>
+      )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.PropTypes = {
+  repos: PropTypes.array.isRequired,
+}
+
+SelectLanguage.PropTypes = {
+  selectedLanguage : PropTypes.string.isRequired,
+  onSelect : PropTypes.func.isRequired,
+}
+
+
+class Popular extends React.Component{
+  constructor(props){
+    super();
+    this.state = {
+      selectedLanguage: 'All',
+      repos: null,
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang){
+    this.setState(function(){
+      return {
+        selectedLanguage: lang,
+        repos: null
+      }
+    })
+
+    api.fetchPopularRepos(lang)
+    .then(function(repos){
+      this.setState(function() {
+        return {
+          repos: repos
+        }
+      })
+    }.bind(this))
+  }
+
+  componentDidMount(){
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  render(){
+    return(
+      <div>
+        <SelectLanguage
+          selectedLanguage = {this.state.selectedLanguage}
+          onSelect = {this.updateLanguage}
+        />
+        {!this.state.repos
+          ? <p>Loading</p>
+          : <RepoGrid repos={this.state.repos} />}
+      </div>
+    )
+  }
+}
+
+module.exports = Popular;
